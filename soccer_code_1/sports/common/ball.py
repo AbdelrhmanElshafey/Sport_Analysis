a330115c15648ea4a0606657ad0561d2d6b8c2af
alyaee <alielsharawy1999@gmail.com>
2025-04-20 16:59:22 +0200

    updated sports/common/ball.py in soccer1: fixed the flickering happening with ball detections in balltracker() class

--- soccer_code_1/sports/common/ball.py
+++ soccer_code_1/sports/common/ball.py
@@ -78,26 +78,24 @@ class BallTracker:
     """
     def __init__(self, buffer_size: int = 10):
         self.buffer = deque(maxlen=buffer_size)
+        self.last_detection = None
 
     def update(self, detections: sv.Detections) -> sv.Detections:
-        """
-        Updates the buffer with new detections and returns the detection closest to the
-        centroid of recent positions.
-
-        Args:
-            detections (sv.Detections): The current frame's ball detections.
-
-        Returns:
-            sv.Detections: The detection closest to the centroid of recent positions.
-            If there are no detections, returns the input detections.
-        """
         xy = detections.get_anchors_coordinates(sv.Position.CENTER)
-        self.buffer.append(xy)
-
+    
         if len(detections) == 0:
-            return detections
-
+            # No detections found
+            if self.last_detection is not None:
+                return self.last_detection
+            else:
+                return detections 
+    
+        self.buffer.append(xy)
+    
         centroid = np.mean(np.concatenate(self.buffer), axis=0)
         distances = np.linalg.norm(xy - centroid, axis=1)
         index = np.argmin(distances)
-        return detections[[index]]
+    
+        selected_detection = detections[[index]]
+        self.last_detection = selected_detection
+        return selected_detection
